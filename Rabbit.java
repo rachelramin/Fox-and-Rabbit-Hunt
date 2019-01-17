31d11ce990ddfff772d75e0e9a0ee89463a017e1
Jared Gross <32721542+jgross979@users.noreply.github.com>
2019-01-17 12:51:08 -0500

    Fixed bug with fox at east.
    Also added random movement when no bush is found.

--- Rabbit.java
+++ Rabbit.java
@@ -9,11 +9,6 @@ public class Rabbit extends Animal {
     private int directionToEdge;
 
 
-//Issues: Bush near edge makes rabbit get stuck
-    //Rabbit does not adjust properly when at corner and near other bushes
-          
-
-
     public Rabbit(Model model, int row, int column) {
         super(model, row, column);
     }
@@ -64,6 +59,7 @@ public class Rabbit extends Animal {
     	
     }
     
+    //Checks what row a given bush is (0-19)
     int checkBushRow(int bushDirection, int bushDistance) {
     	if(bushDirection == Model.E || bushDirection == Model.W) {
     		return this.row;
@@ -76,6 +72,7 @@ public class Rabbit extends Animal {
     	}
     }
     
+    //Checks what column a bush is (0-19)
     int checkBushCol(int bushDirection, int bushDistance) {
     	if(bushDirection == Model.N || bushDirection == Model.S) {
     		return this.column;
@@ -88,6 +85,7 @@ public class Rabbit extends Animal {
     	}
     }
     
+    //Returns true if bush is on an edge
     boolean isEdgeBush(int bushDirection, int bushDistance) {
     	int bushCol = checkBushCol(bushDirection, bushDistance);
     	int bushRow = checkBushRow(bushDirection, bushDistance);
@@ -114,22 +112,37 @@ public class Rabbit extends Animal {
                     	//Handles if close to the edge and can't
                     	//move directly 180 degrees away
                     	for(int j = Model.MIN_DIRECTION; j <= Model.MAX_DIRECTION; j++) {
-                    		if(canMove(awayFromEdge + j)) {
-                    			return awayFromEdge +j;
+                    		int newDirection = Model.turn(awayFromEdge, j);
+                    		if(canMove(newDirection)) {
+                    			return newDirection;
                     		}
                     	}
                     }
                     
                 }
             }
+    //Once away from edge, finds the closest bush
     		
-    		//Otherwise move towards bush
+    		//Finds closest bush, not on an edge
     		for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
                 if (look(i) == Model.BUSH && distance(i) < shortestBushDistance && !isEdgeBush(i, distance(i))) {
                     shortestBushDistance = distance(i);
                     directionToBush = i;
                 }
             }
+    		
+    		//If rabbit cannot see a bush, move in a random direction
+    		if(shortestBushDistance == 100) {
+    			int currentDirection = Model.random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
+    				for (int i = 0; i < 8; i++) {
+    					if (canMove(currentDirection)) {
+    					return currentDirection;
+    					}else {
+    					currentDirection = Model.turn(currentDirection, 1);
+    					}
+    				}
+    		}
+    		//Moves to next phase if rabbit gets to bush
     		//Makes sure rabbit is in proper placement around the bush
     		if(shortestBushDistance <= 1 && !checkBushPlacement()) {
     			atBush = true;
@@ -139,14 +152,15 @@ public class Rabbit extends Animal {
     		}else if(shortestBushDistance <= 1) {
     			atBush = true;
     		}
+    		//Moves towards bush if not next to it
     		return directionToBush;
     	}
     	
     	
-    	//If at the bush, look for the fox
+    	//Once at the bush, look for the fox
     	canSeeFoxNow = false;
     	//Look around for the fox
-    	//If fox is found, direction to fox is set
+    	//If fox is found, direction and distance to fox are set
         for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
             if (look(i) == Model.FOX) {
                 canSeeFoxNow = true;
@@ -155,7 +169,7 @@ public class Rabbit extends Animal {
             }
         }
    
-    	//If the fox is one away, move opposite around the bush
+    	//If the fox is seen, move opposite around the bush
         if(canSeeFoxNow) {
         	//Fox is NORTH of rabbit
         	if(directionToFox == Model.N && directionToBush == Model.E) {
@@ -169,7 +183,7 @@ public class Rabbit extends Animal {
         	else if(directionToFox == Model.E && directionToBush != Model.N) {
         		return moveCounterClockwise();
         	}else if(directionToFox == Model.E && directionToBush == Model.N) {
-        		return moveCounterClockwise();
+        		return moveClockwise();
         	}
         	//Fox is WEST of rabbit
         	else if(directionToFox == Model.W && directionToBush != Model.N) {
